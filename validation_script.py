8bb4ad5a2d987814dd29d1d8dce9ba5e66f2dbcf
Bacon9629 <qwe43213652@gmail.com>
2024-10-13 17:26:02 +0800

    製作比賽影片code完成、地圖2尚未匯入

--- validation_script.py
+++ validation_script.py
@@ -1,7 +1,10 @@
 from time import sleep
 import sys
+import os
 
+import cv2
 import gymnasium
+import numpy as np
 from matplotlib import pyplot as plt
 
 import racecar_gym.envs.gym_api  # Necessary!!!Cannot be deleted!!!
@@ -10,13 +13,11 @@ from agent.Agent import get_valid_agent
 
 if 'racecar_gym.envs.gym_api' not in sys.modules:
     raise RuntimeError('Please run: pip install -e . and import racecar_gym.envs.gym_api first!')
-
-
 def main():
     # ======================================================================
     # Create the environment
     # ======================================================================
-    render_mode = 'human'  # 'human', 'rgb_array_birds_eye' and 'rgb_array_follow'
+    render_mode = 'rgb_array_follow'  # 'human', 'rgb_array_birds_eye' and 'rgb_array_follow'
     env = gymnasium.make(
         'SingleAgentAustria-v0',
         render_mode=render_mode,
@@ -25,8 +26,15 @@ def main():
         # scenario='scenarios/validation2.yml',   # Use this during the midterm competition, ONLY USE THIS FOR VALIDATION
     )
     done = False
-    agent = get_valid_agent()
-
+    agent = get_valid_agent("PPO")
+    # video_path = fr"Z:\Student_Work\_Share\RL競賽影片\{_map}\{student_id}.mp4"
+    video_path = fr"videos/{_map}/{student_id}.mp4"
+    os.makedirs(os.path.dirname(video_path), exist_ok=True)
+    output_video_writer = cv2.VideoWriter(filename=video_path,
+                                          fourcc=cv2.VideoWriter_fourcc('m', 'p', '4', 'v'),
+                                          fps=20,
+                                          frameSize=(640, 480))
+    print("save at", video_path)
     # ======================================================================
     # Run the environment
     # ======================================================================
@@ -39,25 +47,38 @@ def main():
         action = agent.get_action(obs)
         obs, rewards, done, truncated, states = env.step(action)
 
-        if t % 30 == 0 and "rgb" in render_mode:
+        if t % 5 == 0 and "rgb" in render_mode:
             # ==================================
             # Render the environment
             # ==================================
 
-            image = env.render()
-            plt.clf()
-            plt.title("Pose")
-            plt.imshow(image)
-            plt.pause(0.01)
-            plt.ioff()
+            image = env.render().astype(np.uint8)
+
+            text = f"Student ID: {student_id}"
+            cv2.putText(image, text, (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 0), 3)
+
+            # 將影像寫入視頻
+            output_video_writer.write(image)
+
+            # 顯示影像
+            # cv2.imshow(f"{student_id}", cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
+            # cv2.waitKey(1)
+        if t > 200:
+            break
 
         t += 1
         if done or truncated:
             break
 
+        print(f"\r{t} / 12000", end="")
+
     env.close()
+    output_video_writer.release()
+    print("finish", video_path)
 
 
 if __name__ == '__main__':
-
+    _map = "validation1"
+    # _map = "validation2"
+    student_id = "M11252014"
     main()
