61d51c22b0b592183bd1395b0940d9d4a1420b2a
Bacon9629 <qwe43213652@gmail.com>
2024-10-13 18:03:34 +0800

    修改執行目錄讓他吃的到model weight旱地圖

--- validation_script.py
+++ validation_script.py
@@ -15,6 +15,8 @@ import racecar_gym.envs.gym_api  # Necessary!!!Cannot be deleted!!!
 
 if 'racecar_gym.envs.gym_api' not in sys.modules:
     raise RuntimeError('Please run: pip install -e . and import racecar_gym.envs.gym_api first!')
+
+
 def main():
     sys.path.append(AGENT_ROOT_DIR)
     from agent.Agent import get_valid_agent
@@ -22,17 +24,17 @@ def main():
     # ======================================================================
     # Create the environment
     # ======================================================================
-    render_mode = 'rgb_array_follow'  # 'human', 'rgb_array_birds_eye' and 'rgb_array_follow'
+    render_mode = 'rgb_array_birds_eye'  # 'human', 'rgb_array_birds_eye' and 'rgb_array_follow'
     env = gymnasium.make(
         'SingleAgentAustria-v0',
         render_mode=render_mode,
         # scenario='scenarios/circle_cw.yml',  # change the scenario here (change map)
-        scenario=f'scenarios/{_map}.yml',  # change the scenario here (change map)
+        scenario=f'{os.path.dirname(__file__)}/scenarios/{_map}.yml',  # change the scenario here (change map)
         # scenario='scenarios/validation.yml',  # change the scenario here (change map), ONLY USE THIS FOR VALIDATION
         # scenario='scenarios/validation2.yml',   # Use this during the midterm competition, ONLY USE THIS FOR VALIDATION
     )
     done = False
-    agent = get_valid_agent("PPO")
+    agent = get_valid_agent(agent_model)
     # video_path = fr"Z:\Student_Work\_Share\RL競賽影片\{_map}\{student_id}.mp4"
     video_path = fr"{save_root}/{_map}/{student_id}_{name}.mp4"
     os.makedirs(os.path.dirname(video_path), exist_ok=True)
@@ -68,11 +70,13 @@ def main():
             # 將影像寫入視頻
             output_video_writer.write(image)
 
-            # 顯示影像
-            cv2.imshow(f"{student_id}", cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
-            cv2.waitKey(1)
-        if t > 100:
-            break
+            if t % 20 == 0:
+                # 顯示影像
+                cv2.imshow(f"{student_id}", cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
+                cv2.waitKey(1)
+
+        # if t > 100:
+        #     break
 
         t += 1
         if done or truncated:
@@ -88,13 +92,18 @@ def main():
 
 if __name__ == '__main__':
     import re
+
     AGENT_ROOT_DIR = f"Z:\Student_Work\_Share\RL鴻耀\M11352012@ 鄭語萱_931492_assignsubmission_file\M11352012\M11352012"
-    save_root = f"videos"
+    save_root = fr"E:\python\racecar_gym\videos"
     _map = "validation1"
+    # agent_model = "DQN"
+    agent_model = "PPO"
     # _map = "validation2"
 
     # --- dont change ---
     student_id = AGENT_ROOT_DIR.split("\\")[-1]
     name = re.search(r"@ (.*?)_", AGENT_ROOT_DIR).group(1)
+
+    os.chdir(AGENT_ROOT_DIR)
 
     main()
